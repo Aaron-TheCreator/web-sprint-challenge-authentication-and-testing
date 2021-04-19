4c92fcfbb7caa73abc19ba937aa50959aeffaff5
Avery Burns <a.aburns95@gmail.com>
2021-04-19 17:26:45 -0500

    endpoints working, auth working, tests failing, wip

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const errorHandler = require('./auth/errorHandler.js');
 
 const restrict = require('./middleware/restricted.js');
 
@@ -10,16 +11,19 @@ const jokesRouter = require('./jokes/jokes-router.js');
 const server = express();
 
 server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
 
 server.get('/', (req, res) => {
     res.status(200).json({
         message: "Welcome to Auth API"
     })
-})
+});
+
+server.use(errorHandler);
 
 module.exports = server;
